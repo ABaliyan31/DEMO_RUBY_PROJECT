7e5a2dcb8c885741c94f000123d5e1ad6267a1e9
archit <archit.baliyan@1mg.com>
2023-07-24 17:34:23 +0530

    commit sampleapplication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_page/home'
+  get 'static_page/help'
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_page#home'
 
   # Defines the root path route ("/")
   # root "articles#index"
