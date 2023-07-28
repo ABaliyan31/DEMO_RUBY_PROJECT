0613a2174a9399d720d854ce5d915b5a97db1871
archit <archit.baliyan@1mg.com>
2023-07-28 17:14:54 +0530

    commit for email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name, uniqueness: true, presence: true
+  VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+  validates :email , presence: true,uniqueness:{case_sensetive:false},
+    format:{with:VALID_EMAIL_REGEX,multiline:true}
+
 end
