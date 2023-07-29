6ecd3ed52375ee0dec7ad3275bd742999502adb1
archit <archit.baliyan@1mg.com>
2023-07-29 12:53:02 +0530

    ch - 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
 
+  before_save { self.email = email.downcase }
+
   has_many :microposts, dependent: :destroy
+
   validates :name, uniqueness: true, presence: true
+
   VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
-  validates :email , presence: true,uniqueness:{case_sensetive:false},
-    format:{with:VALID_EMAIL_REGEX,multiline:true}
+
+  validates :email , presence: true,uniqueness:{case_sensetive:false}, format:{with:VALID_EMAIL_REGEX,multiline:true}
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+  get '/signup', to: 'users#new'
   resources :microposts do
     resources :comments
   end
 
   resources :users
+
 end
