467eba3077a6c165769f2e2209e283306cf7f3db
archit <archit.baliyan@1mg.com>
2023-07-31 22:13:20 +0530

    first commit of recyclebin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
 
-  # GET /users or /users.json
 
+  def recycle_bin
+
+  end
+
+  # GET /users or /users.json
   def index
     @users = User.all
   end
